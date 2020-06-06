ca6d64a5d24e263575827ce110f9fdfdf50d01bb
NikolayDruzhinin <northernnikolay@gmail.com>
2020-06-06 13:27:30 +0300

    добавлено несколько функций

--- sandbox/src/main/java/MyFirst.java
+++ sandbox/src/main/java/MyFirst.java
@@ -2,6 +2,22 @@
 public class MyFirst {
 
   public static void main(String[] args) {
-    System.out.println("hello,world");
+    hello("world");
+    hello("pip");
+    hello("Anna");
+    int a = 7;
+    int b = 9;
+    int l = 6;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b) );
+  }
+  public static void hello (String somebody) {
+    System.out.println("hello "+somebody);
+  }
+  public static int area (int p) {
+    return p*p;
+  }
+  public static int area (int a, int b) {
+    return a*b;
   }
 }
